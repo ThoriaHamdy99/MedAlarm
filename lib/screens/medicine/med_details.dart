86541b0f424209be7bf4ec1b86f92e11ce5b4b32
Eslam Mohamed <em2000999@gmail.com>
2021-07-15 13:17:05 +0200

    Update Shrouk's part

--- lib/screens/medicine/med_details.dart
+++ lib/screens/medicine/med_details.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_rounded_date_picker/rounded_picker.dart';
+import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
+
+TextEditingController medNameController = new TextEditingController();
 
 class MedDetails extends StatefulWidget {
   static const id = 'MED_DETAILS_SCREEN';
+
   @override
   _MedDetailsState createState() => _MedDetailsState();
 }
@@ -16,38 +22,33 @@ class _MedDetailsState extends State<MedDetails> {
           color: Color(0xFFF8F4F4),
         ),
         centerTitle: true,
-        /*title: Text(
-            "Add New Medicine",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-            ),
-          ),*/
+        title: Text(
+          "Medicine Details",
+          style: TextStyle(
+            fontFamily: "Angel",
+            fontSize: 32,
+            color: Colors.white,
+          ),
+        ),
         elevation: 0.0,
       ),
       body: Container(
-        // padding: EdgeInsets.all(10),
         color: Theme.of(context).accentColor,
         child: Column(
-          children: <Widget>[
+          children: [
             Container(
               color: Theme.of(context).accentColor,
 
               // height: 200,
-              child: Padding(
-                padding: EdgeInsets.only(
-                    //bottom: 10,
-                    //top: 10,
-                    ),
-                child: Text(
-                  "Medicine Details",
-                  style: TextStyle(
-                    fontFamily: "Angel",
-                    fontSize: 32,
-                    color: Colors.white,
+              /*child:Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 10,
+                    top: 10,
                   ),
-                ),
-              ),
+                child:SizedBox(
+                  height: 50,
+                ) ,
+                ),*/
             ),
             Flexible(
               flex: 3,
@@ -56,6 +57,33 @@ class _MedDetailsState extends State<MedDetails> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        elevation: 10,
+        foregroundColor: Colors.white,
+        backgroundColor: Theme.of(context).accentColor,
+        label: Row(
+          children: <Widget>[
+            Text(
+              " Next ",
+              style: TextStyle(
+                fontFamily: "Angel",
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+            Icon(Icons.arrow_forward)
+          ],
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MedReminderDetails(),
+            ),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
@@ -77,14 +105,14 @@ class _BottomContainerState extends State<_BottomContainer> {
         height: 800,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(50),
-            topRight: Radius.circular(50),
+            topLeft: Radius.elliptical(50, 27),
+            topRight: Radius.elliptical(50, 27),
           ),
           color: Colors.white,
         ),
         width: double.infinity,
         child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 18, vertical: 30),
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -94,7 +122,8 @@ class _BottomContainerState extends State<_BottomContainer> {
                     isRequired: true,
                   ),
                   TextFormField(
-                    maxLength: 1,
+                    controller: medNameController,
+                    maxLines: 1,
                     style: TextStyle(
                       fontSize: 16,
                     ),
@@ -162,8 +191,7 @@ class _BottomContainerState extends State<_BottomContainer> {
                     },
                   ),
                   PanelTitle(
-                    title:
-                        "Dose (The amount of medicine in one dose)",
+                    title: "amount of med in one dose",
                     isRequired: true,
                   ),
                   TextFormField(
@@ -171,7 +199,7 @@ class _BottomContainerState extends State<_BottomContainer> {
                     style: TextStyle(
                       fontSize: 16,
                     ),
-                    maxLength: 1,
+                    maxLines: 1,
                     decoration: InputDecoration(
                       border: UnderlineInputBorder(),
                       labelText: 'Enter number',
@@ -221,14 +249,184 @@ class PanelTitle extends StatelessWidget {
           TextSpan(
             text: title,
             style: TextStyle(
-                fontSize: 16, color: Colors.black, fontWeight: FontWeight.w500),
+                fontSize: 24, color: Colors.black, fontWeight: FontWeight.w500),
           ),
           TextSpan(
             text: isRequired ? " *" : "",
-            style: TextStyle(fontSize: 14, color: Colors.red),
+            style: TextStyle(fontSize: 16, color: Colors.red),
           ),
         ]),
       ),
     );
+  }
+}
+
+class MedReminderDetails extends StatefulWidget {
+  @override
+  _MedReminderDetailsState createState() => _MedReminderDetailsState();
+}
+
+class _MedReminderDetailsState extends State<MedReminderDetails> {
+  String medname = medNameController.text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).accentColor,
+        iconTheme: IconThemeData(
+          color: Color(0xFFF8F4F4),
+        ),
+        centerTitle: true,
+        title: Text(
+          "$medname Details",
+          style: TextStyle(
+            fontFamily: "Angel",
+            fontSize: 32,
+            color: Colors.white,
+          ),
+        ),
+        elevation: 0.0,
+      ),
+      body: Container(
+        color: Theme.of(context).accentColor,
+        child: Flexible(
+          flex: 3,
+          child: _ReminderDetailsContainer(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        elevation: 10,
+        foregroundColor: Colors.white,
+        backgroundColor: Theme.of(context).accentColor,
+        label: Text(
+          " Done ",
+          style: TextStyle(
+            fontFamily: "Angel",
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MedReminderDetails(),
+            ),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class _ReminderDetailsContainer extends StatefulWidget {
+  @override
+  _ReminderDetailsContainerState createState() =>
+      _ReminderDetailsContainerState();
+}
+
+class _ReminderDetailsContainerState extends State<_ReminderDetailsContainer> {
+  var _dateTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('en', 'US'), // English
+          // const Locale('th', 'TH'), // Thai
+        ],
+        home: Scaffold(
+            backgroundColor: Theme.of(context).accentColor,
+            body: Container(
+                // height: 800,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                  color: Colors.white,
+                ),
+                width: double.infinity,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: PanelTitle(
+                            title: "Pick up Start date",
+                            isRequired: true,
+                          )),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(60.0),
+                          ),
+                          primary: Theme.of(context).accentColor, // background
+                          onPrimary: Colors.amber, // foreground
+                        ),
+                        child: new Icon(Icons.date_range),
+                        onPressed: () => showRoundedDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(DateTime.now().year),
+                          lastDate: DateTime(DateTime.now().year + 2),
+                          borderRadius: 16,
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: PanelTitle(
+                            title: "Pick up end date",
+                            isRequired: true,
+                          )),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(60.0),
+                          ),
+                          primary: Theme.of(context).accentColor, // background
+                          onPrimary: Colors.amber, // foreground
+                        ),
+                        child: new Icon(Icons.date_range),
+                        onPressed: () => showRoundedDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(DateTime.now().year),
+                          lastDate: DateTime(DateTime.now().year + 2),
+                          borderRadius: 16,
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: PanelTitle(
+                            title: "Pick up time for first reminder",
+                            isRequired: true,
+                          )),
+                      TimePickerSpinner(
+                        is24HourMode: false,
+                        normalTextStyle: TextStyle(
+                            fontSize: 24, color: Theme.of(context).accentColor),
+                        highlightedTextStyle:
+                            TextStyle(fontSize: 24, color: Colors.amber),
+                        spacing: 50,
+                        itemHeight: 80,
+                        isForce2Digits: true,
+                        onTimeChange: (time) {
+                          setState(() {
+                            _dateTime = time;
+                          });
+                        },
+                      )
+                    ]))));
   }
 }
